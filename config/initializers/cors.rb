9f3c7df307e9d02dfd9c6cc5e90ab200201eee76
christine <bumpouce@gmail.com>
2020-08-20 12:33:56 -0700

    working on cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       # origins '*'
-      origins 'https://pair-up.netlify.app/'
+      origins 'https://pair-up.netlify.app/*'
 
       # whitelist netlify url
    
