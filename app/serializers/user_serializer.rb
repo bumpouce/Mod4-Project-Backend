445dff49d7897dc2b1ae87f1631e50805074ada0
christine <bumpouce@gmail.com>
2020-08-20 11:50:24 -0700

    fixed merge issue

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
-    attributes :username, :country, :avatar
- 
-=======
     attributes :username, :country, :avatar    
->>>>>>> 7d86e8ed4db3a9201dd18bc9ecbc23467d2968da
   end
