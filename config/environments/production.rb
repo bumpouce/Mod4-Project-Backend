18367b1be13115c30478230d281e20f139c48882
christine <bumpouce@gmail.com>
2020-08-20 12:22:01 -0700

    continued work on application.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,10 +72,17 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  # if ENV["RAILS_LOG_TO_STDOUT"].present?
+  #   logger           = ActiveSupport::Logger.new(STDOUT)
+  #   logger.formatter = config.log_formatter
+  #   config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  # end
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
+  config.log_level = ENV.fetch('LOG_LEVEL', 'INFO').downcase
+
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
+    logger.level     = ActiveSupport::Logger.const_get(config.log_level.to_s.upcase)
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
