4aeb643c3a731fa5f4947034988fff8c3532f5d5
christine <bumpouce@gmail.com>
2020-08-20 13:53:39 -0700

    added static landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get '/current_user', to: 'application#current_user'
+  root 'welcome#index'
   get '/check_user', to: 'application#check_user'
-  root 'users#newgame'
   namespace :api do
     namespace :v1 do
       resources :users #, only: [:create, :show]
